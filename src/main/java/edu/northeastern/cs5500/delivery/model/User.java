eee01059299bc14c04d5a7b14ad55ea1292fdd3f
Nachiket Dani <8498689+NachiketDani@users.noreply.github.com>
2020-10-27 01:55:59 -0700

    Cleaned Restaurant Model, added restaurant controller and default methods

--- src/main/java/edu/northeastern/cs5500/delivery/model/User.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/User.java
@@ -1,7 +1,4 @@
 package edu.northeastern.cs5500.delivery.model;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
-import org.bson.types.ObjectId;
 
 public class User extends Person {
     private String address;
@@ -9,16 +6,26 @@ public class User extends Person {
     private String state;
     private String zip;
 
-    public User(String username, String firstName, String lastName, String email, Integer phoneNumber, String address, String city, String state, String zip) {
+    public User(
+            String username,
+            String firstName,
+            String lastName,
+            String email,
+            Integer phoneNumber,
+            String address,
+            String city,
+            String state,
+            String zip) {
         super(username, firstName, lastName, email, phoneNumber);
         this.address = address;
         this.city = city;
         this.state = state;
         this.zip = zip;
     }
-    
+
     /**
      * Getter for address
+     *
      * @return address as a string
      */
     public String getAddress() {
@@ -27,6 +34,7 @@ public class User extends Person {
 
     /**
      * getter for city
+     *
      * @return city as a string
      */
     public String getCity() {
@@ -35,6 +43,7 @@ public class User extends Person {
 
     /**
      * getter for state
+     *
      * @return state as a string
      */
     public String getState() {
@@ -43,36 +52,29 @@ public class User extends Person {
 
     /**
      * getter for zip
+     *
      * @return zip as a string
      */
     public String getZip() {
         return zip;
     }
 
-    /**
-     * sets address based on method argument
-     */
+    /** sets address based on method argument */
     public void setAddress(String address) {
         this.address = address;
     }
 
-    /**
-     * sets city based on method argument
-     */
+    /** sets city based on method argument */
     public void setCity(String city) {
         this.city = city;
     }
 
-    /**
-     * sets state based on method argument
-     */
+    /** sets state based on method argument */
     public void setState(String state) {
         this.state = state;
     }
 
-    /**
-     * sets zip based on method argument
-     */
+    /** sets zip based on method argument */
     public void setZip(String zip) {
         this.zip = zip;
     }
