19a3e2e272202b1a7f0ae8256741436b66515851
chiangp <chiangp@husky.neu.edu>
2020-11-05 11:49:09 -0800

    Changed block comment in RepositoryModule Conflict

--- src/main/java/edu/northeastern/cs5500/delivery/repository/RepositoryModule.java
+++ src/main/java/edu/northeastern/cs5500/delivery/repository/RepositoryModule.java
@@ -31,18 +31,24 @@ public class RepositoryModule {
 }
 
 /*
-Here's an example of how you imght swap out the in-memory repository for a
-database-backed // repository:
-
+// Here's an example of how you imght swap out the in-memory repository for a database-backed
+// repository:
 package edu.northeastern.cs5500.delivery.repository;
+import dagger.Module;
+import dagger.Provides;
+import edu.northeastern.cs5500.delivery.model.Delivery;
+import edu.northeastern.cs5500.delivery.service.MongoDBService;
 
-import dagger.Module; import dagger.Provides; import
-edu.northeastern.cs5500.delivery.model.Delivery; import
-edu.northeastern.cs5500.delivery.service.MongoDBService;
+    @Module public class RepositoryModule {
+    @Provides
+    public GenericRepository<Delivery> provideDeliveryRepository(MongoDBService mongoDBService) {
+        return new MongoDBRepository<>(Delivery.class, mongoDBService);
+    }
 
-@Module public class RepositoryModule {
-
-@Provides public GenericRepository<Delivery>
-provideDeliveryRepository(MongoDBService mongoDBService) { return new
-MongoDBRepository<>(Delivery.class, mongoDBService); } }
+    @Module public class RepositoryModule {
+    @Provides
+    public GenericRepository<Restaurant> provideRestaurantRepository(MongoDBService mongoDBService) {
+        return new MongoDBRepository<>(Restaurant.class, mongoDBService);
+    }
+}
 */
